be09ff13664891e34ef7ce9816f5805a09ca6f86
mitsuru-sekiguchi <roth_peyo@icloud.com>
2021-10-18 17:26:32 +0900

    Add a login status management process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :autenticate
   helper_method :logged_in?
 
   private
 
   def logged_in?
-    !!session[:user_id]
+    !!current_user
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def autenticate
+    return if loggef_in?
+    redirect_to root_path, alert: "ログインして下さい"
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  skip_before_action :autenticate
   def index
   end
 end
