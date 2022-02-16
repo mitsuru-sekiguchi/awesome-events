3b9f2343bdc1bc50e530c86e2cdb8fbe7ed74020
mitsuru-sekiguchi <roth_peyo@yahoo.co.jp>
2022-02-16 18:18:06 +0900

    production.rb fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,5 +109,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['SEARCHBOX_URL'], http: { port: 443, scheme: 'https' })
 end
