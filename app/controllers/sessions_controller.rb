9928a5a651d6401f71786ae4dd18292040e47b06
mitsuru-sekiguchi <roth_peyo@yahoo.co.jp>
2022-02-23 16:34:37 +0900

    line-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,14 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate, only: :create
 
   def create
-    user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_path, notice: "ログインしました"
+    user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    if user
+      session[:user_id] = user.id
+      session[:user_name] = user.name
+      redirect_to root_path, notice: "ログインしました"
+    else
+      redirect_to root_path, notice: "失敗しました"
+    end
   end
 
   def destroy
