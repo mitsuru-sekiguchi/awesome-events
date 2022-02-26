112ea6e88f51f1a171b9b2419360fdcb7bc088ae
mitsuru-sekiguchi <roth_peyo@yahoo.co.jp>
2022-02-26 18:21:43 +0900

    header fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,9 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate, only: :create
 
   def create
-    user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    user = User.find_or_create_from_auth_hash!(request.env['omniauth.auth'])
     if user
       session[:user_id] = user.id
-      session[:user_name] = user.name
       redirect_to root_path, notice: "ログインしました"
     else
       redirect_to root_path, notice: "失敗しました"
