f23aced05e7226f7686601689e2e4decbb43e699
mitsuru-sekiguchi <roth_peyo@yahoo.co.jp>
2022-02-28 21:10:06 +0900

    add a top page back btn

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -5,4 +5,5 @@ if Rails.env.development?
 
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
+  Rack::MiniProfiler.config.start_hidden = true
 end
