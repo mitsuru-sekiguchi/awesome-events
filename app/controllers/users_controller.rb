093cbd3a3300f3a5a2208af64053fa875e2ff19a
mitsuru-sekiguchi <roth_peyo@yahoo.co.jp>
2022-03-01 12:45:47 +0900

    add a user controller and view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def list
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/index'
+  end
+  get 'users/list'
+  get 'users/show'
+  get 'users/edit'
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy", as: :logout
