4b46789da6f74b70a65abe9203b07de9ab68b25f
mitsuru-sekiguchi <roth_peyo@icloud.jp>
2022-05-28 17:18:47 +0900

    multiple registrations of owner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_003714) do
+ActiveRecord::Schema.define(version: 2022_05_28_074303) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2022_04_13_003714) do
   end
 
   create_table "event_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "user_id"
+    t.string "user_id"
     t.bigint "event_id", null: false
     t.bigint "owner_id"
     t.datetime "created_at", precision: 6, null: false
